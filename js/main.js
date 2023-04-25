c3387483b6c05635882dc49ea6cd2984a5a54d80
Yan Araujo <saresende555@gmail.com>
2023-04-25 14:34:02 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+    });
+
+    $('.menu-hamburguer').click(function () {
+        $('nav').slideToggle();
+    })
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('#cpf').mask('000-000-000-00')
+    
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            }
+
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function (form) {
+            console.log(form)
+        },
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+    $('.lista-veiculos button').click(function () {
+        const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
+
+        $('#veiculo-interesse').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+})
